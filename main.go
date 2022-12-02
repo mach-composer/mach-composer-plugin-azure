f56e1766f78b7f5e5d8d5cf2380e34179a93fb60
Michael van Tellingen <michaelvantellingen@gmail.com>
2022-12-02 08:43:21 +0100

    Initial export from mach-composer repository

---
+++ main.go
@@ -0,0 +1,12 @@
+package azure
+
+import (
+	"github.com/mach-composer/mach-composer-plugin-sdk/plugin"
+
+	"github.com/mach-composer/mach-composer-plugin-azure/internal"
+)
+
+func main() {
+	p := internal.NewAzurePlugin()
+	plugin.ServePlugin(p)
+}
